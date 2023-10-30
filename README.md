010ef2e914e164963c5867282f27056dd1de972d
Sami Nassim <sami.bououdine@gmail.com>
2023-10-30 08:45:08 +0100

    Added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculateur d'âge
+
+Mon tout premier programme Java : un calculateur d'âge.</br>
+L'utilisateur entre sa date de naissance au format JJ/MM/AAAA via le terminal et sa date de naissance est calculée au nombre de jours près.</br>
+Programme assez simple, c'était uniquement pour me familiariser avec IntelliJ et la syntaxe Java.
--- src/com/agecalculator/Main.java
+++ src/com/agecalculator/Main.java
@@ -5,8 +5,6 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
